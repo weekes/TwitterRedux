8925ded1d1cc81e7c0d23819c9af2888acd7cb46
Marcel Weekes <weekes@gmail.com>
2016-02-25 00:17:27 -0800

    Trying frameworks instead of dynamic libraries (again)

--- Podfile
+++ Podfile
@@ -1,4 +1,12 @@
+# Uncomment this line to define a global platform for your project
 platform :ios, '9.0'
 
-pod "AFNetworking"
-pod "BDBOAuth1Manager"
+# Uncomment this line if you're using Swift
+use_frameworks!
+
+target 'Twitter' do
+
+pod 'AFNetworking', '~> 3.0'
+pod 'BDBOAuth1Manager', '~> 2.0'
+
+end
--- Pods/Target Support Files/Pods-Twitter/Pods-Twitter-frameworks.sh
+++ Pods/Target Support Files/Pods-Twitter/Pods-Twitter-frameworks.sh
@@ -82,3 +82,12 @@ strip_invalid_archs() {
   fi
 }
 
+
+if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "Pods-Twitter/AFNetworking.framework"
+  install_framework "Pods-Twitter/BDBOAuth1Manager.framework"
+fi
+if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "Pods-Twitter/AFNetworking.framework"
+  install_framework "Pods-Twitter/BDBOAuth1Manager.framework"
+fi
